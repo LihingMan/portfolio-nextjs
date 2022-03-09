1d09ef878779782ab9fa517ec996d23eb1605194
Bryan Lee <bryanleee134@gmail.com>
2022-03-09 22:15:02 +0800

    Experience page

--- constants/data.js
+++ constants/data.js
@@ -24,6 +24,43 @@ const userData = {
       "Deez nuts developer developer developer developer",
     ],
   },
+  experience: [
+    {
+      title: "Software Engineer - Backend Developer",
+      company: "Axflix Technologies Sdn. Bhd.",
+      year: "2021 - 2022",
+      companyLink: "https://www.axflix.com/",
+      desc: "Worked on the backend for multiple projects using Node.js (Express.js) and Django. I've worked on a school management system (DirectEdu) and also a consolidation accounting system for companies",
+    },
+    {
+      title: "Backend Developer",
+      company: "Monash University Malaysia",
+      year: "2020 - 2020",
+      companyLink: "https://www.monash.edu.my/",
+      desc: "Developed a mobile application (HEARD) with a small Scrum team for the deaf community in Malaysia to easily pair up with Sign Language Interpreters.",
+    },
+    {
+      title: "Internship",
+      company: "The Nielsen Company (Malaysia) Sdn. Bhd.",
+      year: "2019 - 2020",
+      companyLink: "",
+      desc: "Applied data cleaning and data manipulation techniques on large operational datasets for further processing.",
+    },
+    {
+      title: "Frontend Developer",
+      company: "123 Tech Solutions",
+      year: "2018 - 2019",
+      companyLink: "",
+      desc: "Created a web-based product configurator for a cupboard made by stacking cubes using a 3D JavaScript engine (Babylon.js).",
+    },
+    {
+      title: "Education",
+      company: "Monash University Malaysia",
+      year: "2016 - 2020",
+      companyLink: "",
+      desc: "Bachelor of Software Engineering (Honours)",
+    },
+  ],
 };
 
 export default userData;
