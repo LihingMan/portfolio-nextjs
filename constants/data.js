60055d2ee9b8bc3897f394dd80414ed9b1b234c6
Bryan Lee <bryanleee134@gmail.com>
2022-03-20 11:40:16 +0800

    update resume link

--- constants/data.js
+++ constants/data.js
@@ -10,7 +10,7 @@ const userData = {
   phone: "+60 109487520",
   address: "Selangor, Malaysia",
   resumeUrl:
-    "https://drive.google.com/file/d/1MyX4YrW7E45LahLZs9N26XWu1xewhzUc/view?usp=sharing",
+    "https://drive.google.com/file/d/1S7aSP_N8kgtl3Jbt16irRDxSMgi1my3M/view?usp=sharing",
   socialLinks: {
     github: "https://github.com/LihingMan",
     linkedin: "https://www.linkedin.com/in/hsiu-wei-lee-bryan-1256571ab/",
